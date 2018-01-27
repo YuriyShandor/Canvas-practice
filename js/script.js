b8fc68e9885c56727dae25d9409a8cf67d4b25fc
Yuriy Shandor <yuriyshandor@gmail.com>
2018-01-27 19:00:47 +0200

    Add Animation. Mod_02

--- js/script.js
+++ js/script.js
@@ -53,51 +53,92 @@ for(var i=0; i<10; i++) {
 //=============== Animating canvas ============
 
 var animateCanvas = document.getElementById('animateCanvas');
-
-animateCanvas.width = 700;
-animateCanvas.height = 500;
-
-// function Circle() {
-//   this.x = x;
-//   this.y = y;
-//
-//   this.draw = function() {
-//     console.log('hello');
-//   }
-// };
-//
-// var circle = new Circle();
-//
-// circle.draw();
-
 var aniCan = animateCanvas.getContext('2d');
 
-var aniCan_x = Math.random() * animateCanvas.width;
-var aniCan_y = Math.random() * animateCanvas.height;
-var aniCan_radius = 30;
-var aniCan_dx = 4;
-var aniCan_dy = 4;
+animateCanvas.width = 900;
+animateCanvas.height = 600;
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = function() {
+    aniCan.beginPath();
+    aniCan.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    aniCan.strokeStyle = "red";
+    aniCan.lineWidth = 5;
+    aniCan.stroke();
+    aniCan.fill();
+  };
+
+  this.move = function() {
+    if (this.x + this.radius > animateCanvas.width || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    };
+
+    if (this.y + this.radius > animateCanvas.height || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    };
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  };
+};
+
+var circleArray = [];
+
+for (var i=0; i<50; i++) {
+  var x = Math.random() * (animateCanvas.width - radius * 2) + radius;
+  var y = Math.random() * (animateCanvas.height - radius * 2) + radius;
+  var radius = 30;
+  var dx = (Math.random() - 0.5) * 7;
+  var dy = (Math.random() - 0.5) * 7;
+
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
 
 function animate() {
   requestAnimationFrame(animate);
   aniCan.clearRect(0, 0, animateCanvas.width, animateCanvas.height);
 
-  aniCan.beginPath();
-  aniCan.arc(aniCan_x, aniCan_y, aniCan_radius, 0, Math.PI * 2, false);
-  aniCan.strokeStyle = "red";
-  aniCan.lineWidth = 5;
-  aniCan.stroke();
-
-  if(aniCan_x + aniCan_radius > animateCanvas.width || aniCan_x - aniCan_radius < 0) {
-    aniCan_dx = -aniCan_dx;
+  for (var i=0; i < circleArray.length; i++) {
+    circleArray[i].move();
   };
-
-  if(aniCan_y + aniCan_radius > animateCanvas.height || aniCan_y - aniCan_radius < 0) {
-    aniCan_dy = -aniCan_dy;
-  };
-
-  aniCan_x += aniCan_dx;
-  aniCan_y += aniCan_dy;
 };
 
 animate();
+
+// var aniCan_x = Math.random() * animateCanvas.width;
+// var aniCan_y = Math.random() * animateCanvas.height;
+// var aniCan_radius = 30;
+// var aniCan_dx = 4;
+// var aniCan_dy = 4;
+//
+// function animate() {
+//   requestAnimationFrame(animate);
+//   aniCan.clearRect(0, 0, animateCanvas.width, animateCanvas.height);
+//
+//   aniCan.beginPath();
+//   aniCan.arc(aniCan_x, aniCan_y, aniCan_radius, 0, Math.PI * 2, false);
+//   aniCan.strokeStyle = "red";
+//   aniCan.lineWidth = 5;
+//   aniCan.stroke();
+//
+//   if(aniCan_x + aniCan_radius > animateCanvas.width || aniCan_x - aniCan_radius < 0) {
+//     aniCan_dx = -aniCan_dx;
+//   };
+//
+//   if(aniCan_y + aniCan_radius > animateCanvas.height || aniCan_y - aniCan_radius < 0) {
+//     aniCan_dy = -aniCan_dy;
+//   };
+//
+//   aniCan_x += aniCan_dx;
+//   aniCan_y += aniCan_dy;
+// };
+//
+// animate();
