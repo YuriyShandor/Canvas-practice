9e3b40675043e918810787d52f24b0edc9b6c063
Yuriy Shandor <yuriyshandor@gmail.com>
2018-01-27 14:03:56 +0200

    Add First Canvas. Mod_02

--- js/script.js
+++ js/script.js
@@ -10,6 +10,40 @@ firstCanvas.height = 500;
 
 var c = firstCanvas.getContext('2d');
 
+//========== Rectangles ==========
+
+//c.fillRect(x, y, width, height);
+
+c.fillStyle = "green";
 c.fillRect(100, 100, 150, 150);
 c.fillRect(400, 400, 50, 50);
+c.fillStyle = "blue";
 c.fillRect(300, 100, 100, 100);
+
+//========== Lines ==========
+
+c.beginPath();
+c.moveTo(50, 350); //beggin
+c.lineTo(500, 200);
+c.lineTo(530, 300);
+c.strokeStyle = "red";
+c.stroke();
+
+//============= Arc / Cidcle =============
+
+//c.ark(x, y, r, sAngle, eAngle);
+
+c.beginPath();
+c.arc(600, 75, 50, 0, Math.PI * 2, false);
+c.strokeStyle = "#000";
+c.stroke();
+
+for(var i=0; i<10; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+
+  c.beginPath();
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = "red";
+  c.stroke();
+}
